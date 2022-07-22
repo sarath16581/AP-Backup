3743e0ff5916cf403d100f0dac4af4710610c11e
Hasantha Liyanage <hasanthaera@gmail.com>
2022-07-22 10:59:03 +1000

    DDS-11533  - StarTrack - UI Layout - Feedback form

--- force-app/main/default/lwc/sthsFeedbackForm/sthsFeedbackForm.js
+++ force-app/main/default/lwc/sthsFeedbackForm/sthsFeedbackForm.js
@@ -1,46 +1,88 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
+import { validateInputComponents } from "c/utils";
 import STHS_ICONS from "@salesforce/resourceUrl/STHS_Icons";
+import invalidDescription from "@salesforce/label/c.Sths_Description_Validation_Message";
+import invalidEmail from "@salesforce/label/c.Sths_Email_Validation_Message";
+import invalidFeedbackTypeSelection from "@salesforce/label/c.Sths_Feedback_Selection_Validation_Message";
+import invalidFeedback from "@salesforce/label/c.Sths_Feedback_Validation_Message";
+import invalidFirstName from "@salesforce/label/c.Sths_Firstname_Validation_Message";
+import invalidLastName from "@salesforce/label/c.Sths_Lastname_Validation_Message";
+import invalidCharacters from "@salesforce/label/c.Sths_Max_Characters_Validation_Message";
+import invalidPhone from "@salesforce/label/c.Sths_Phone_Validation_Message";
+import invalidReference from "@salesforce/label/c.Sths_Reference_Validation_Message";
+import stSupportURL from "@salesforce/label/c.Sths_Support_URL";
 
 export default class SthsTrackingForm extends LightningElement {
-    arrowLeft = STHS_ICONS + "/sths_icons/svgs/forms/arrow_left.svg"; //left arrow
-    stSupportURL = "https://startrack.com.au/support"; //StarTrack support URL
-    showReference = false;
-    REFERENCE_REQUIRED_FEEDBACK_TYPES = ['Product & Sales','Pick Up','On-Road','Delivery'];
-    get enquiryOptions() {
-        return [
-            { label: 'Product & Sales', value: 'Product & Sales' },//
-            { label: 'Account Management', value: 'Account Management' },
-            { label: 'Pick Up', value: 'Pick Up' },//
-            { label: 'On-Road', value: 'On-Road' },//
-            { label: 'Delivery', value: 'Delivery' },//
-            { label: 'Contact Centre', value: 'Contact Centre' },
-            { label: 'Billing', value: 'Billing' },
-            { label: 'Other', value: 'Other' },
-        ];
-    }
+	arrowLeft = STHS_ICONS + "/sths_icons/svgs/forms/arrow_left.svg"; //left arrow
+	showReference = false;
+	REFERENCE_REQUIRED_FEEDBACK_TYPES = [
+		"Product & Sales",
+		"Pick Up",
+		"On-Road",
+		"Delivery"
+	];
+	formData = {}; //form data to capture
 
-    //handle enquiry dropdown change
-    handleEnquiryChange(event){
-        const feedbackType = event.target.value;
-        if(this.REFERENCE_REQUIRED_FEEDBACK_TYPES.includes(feedbackType)) {
-            this.showReference = true;
-        } else {
-            this.showReference = false;
-        }
-    }
+	//labels
+	label = {
+		invalidDescription,
+		invalidEmail,
+		invalidFeedbackTypeSelection, // Please select what your feedback is about
+		invalidFeedback, // Please enter your feedback
+		invalidFirstName,
+		invalidLastName,
+		invalidCharacters,
+		invalidPhone,
+		invalidReference,
+		stSupportURL
+	};
 
-    handleInputChange(event) {
-        this.formData = De(De({}, this.formData), {}, {
-            [event.target.dataset.fieldName]: event.detail.value
-        })
-    }
+	get enquiryOptions() {
+		return [
+			{ label: "Product & Sales", value: "Product & Sales" }, //
+			{ label: "Account Management", value: "Account Management" },
+			{ label: "Pick Up", value: "Pick Up" }, //
+			{ label: "On-Road", value: "On-Road" }, //
+			{ label: "Delivery", value: "Delivery" }, //
+			{ label: "Contact Centre", value: "Contact Centre" },
+			{ label: "Billing", value: "Billing" },
+			{ label: "Other", value: "Other" }
+		];
+	}
 
-    handleSubmitClick(event){
-        this.validateForm() && console.log(JSON.parse(JSON.stringify(this.formData)))
-    }
+	//handle enquiry dropdown change
+	handleEnquiryChange(event) {
+		const feedbackType = event.target.value;
+		if (this.REFERENCE_REQUIRED_FEEDBACK_TYPES.includes(feedbackType)) {
+			this.showReference = true;
+		} else {
+			this.showReference = false;
+		}
+	}
 
-    validateForm() {
-        let elements = this.template.querySelectorAll('[data-form="feedback"]');
-        //return o.validateInputComponents([...elements], !0)
-    }
+	//handler for input type fields
+	handleInputChange(event) {
+		this.formData = {
+			...this.formData,
+			[event.target.dataset.fieldName]: event.detail.value
+		};
+	}
+
+	//handle form submit click
+	handleSubmitClick(event) {
+
+		let isFormValid = this.validateForm();
+		if (isFormValid) {
+			//submit the form
+			console.log(JSON.parse(JSON.stringify(this.formData)));
+		}
+	}
+
+	//validate the form
+	validateForm() {
+		let inputElements = this.template.querySelectorAll(
+			'[data-validation="feedbackForm"]'
+		);
+		return validateInputComponents([...inputElements], true);
+	}
 }
