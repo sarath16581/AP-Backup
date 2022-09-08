9b4e08c3bffe601ae0d1b77c59937929c6fa60fc
Naveen Rajanna <naveen.rajanna@auspost.com.au>
2022-09-08 17:39:33 +1000

    REQ2963906 - myNetwork changes to support Enhanced domain go live

--- force-app/main/default/aura/MyNetworkProfileHeader/MyNetworkProfileHeaderController.js
+++ force-app/main/default/aura/MyNetworkProfileHeader/MyNetworkProfileHeaderController.js
@@ -1,26 +1,24 @@
-({  
-	doInit : function(component, event, helper) {
-   	 helper.getLocalList(component);
-	},
-    
-    homeClick : function(component) {
-		window.location = "/myNetwork/s/";
+({
+	doInit: function (component, event, helper) {
+		helper.getLocalList(component);
 	},
 
-    profileClick : function(component) {
-		var action = component.get("c.myProfileUrl");
-        action.setCallback(this, function(response) {
-            window.location = response.getReturnValue();
-        });
-        $A.enqueueAction(action);
+	homeClick: function (component) {
+		window.location = component.get("v.objClassController").pathPrefix + "/s"; //REQ2963906 included the path prefix if exists
 	},
-    
-    logoutClick : function(component) {
-		var action = component.get("c.myLogoutUrl");
-        action.setCallback(this, function(response) {
-            console.log(response.getReturnValue());
-            window.location = response.getReturnValue();
-        });
-        $A.enqueueAction(action);
-	}
-})
+
+	profileClick: function (component) {
+		//REQ2963906 - removed server call and included the path prefix if exists
+		window.location =
+			component.get("v.objClassController").pathPrefix + "/s/profile/" + component.get("v.objClassController").id;
+	},
+
+	logoutClick: function (component) {
+		//REQ2963906 - removed server call and included the path prefix if exists
+		if (component.get("v.objClassController").retailLogoutURL) {
+			window.location = component.get("v.objClassController").retailLogoutURL;
+		} else {
+			window.location = component.get("v.objClassController").pathPrefix + "/secur/logout.jsp";
+		}
+	},
+});
