fb629b3dd4423381cf79047cde115a5c4996804d
Harry Wang <wml699@gmail.com>
2023-01-31 12:43:55 +1100

    STP-7600 removing credit assessment section from proposal layouts; Updating opportunity lightning page to include Credit Assessment tab (new LWC)

---
+++ force-app/main/default/lwc/creditAssessmentWrapper/creditAssessmentWrapper.js
@@ -0,0 +1,15 @@
+/**
+ * @author Harry Wang
+ * @date 2023-01-30
+ * @group Controller
+ * @tag Controller
+ * @domain CSP
+ * @description Javascript controller for creditAssessmentWrapper
+ * @changelog
+ * 2023-01-30 - Harry Wang - Created
+ */
+import {LightningElement} from 'lwc';
+
+export default class CreditAssessmentWrapper extends LightningElement {
+
+}
