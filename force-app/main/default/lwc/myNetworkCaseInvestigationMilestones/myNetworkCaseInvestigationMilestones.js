95d006699e1e59029125a82059136d51e7f6126a
Mahesh Parvathaneni <mparvathanen@powercor.com.au>
2023-02-08 14:26:40 +1100

    updated the comments

--- force-app/main/default/lwc/myNetworkCaseInvestigationMilestones/myNetworkCaseInvestigationMilestones.js
+++ force-app/main/default/lwc/myNetworkCaseInvestigationMilestones/myNetworkCaseInvestigationMilestones.js
@@ -1,3 +1,10 @@
+/**
+ * @description Renders the current network milestone violation in MyNetwork portal
+ * @author Mahesh Parvathaneni
+ * @date 2023-02-07
+ * @changelog
+ */
+
 import { LightningElement, api, wire } from 'lwc';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import FIELD_NETWORK_MILESTONES_VIOLATED from '@salesforce/schema/CaseInvestigation__c.NetworkMilestonesViolated__c';
@@ -5,6 +12,7 @@ import FIELD_NETWORK_MILESTONE_NEXT_VIOLATION_DATETIME from '@salesforce/schema/
 import FIELD_NETWORK_MILESTONE_LAST_VIOLATION_DATETIME from '@salesforce/schema/CaseInvestigation__c.NetworkMilestoneLastViolationDatetime__c';
 import FIELD_MILESTONE_START_DATETIME from '@salesforce/schema/CaseInvestigation__c.MilestoneTimeStartDateTime__c';
 
+//constants
 const OPEN_VIOLATION = 'Open Violation';
 const REMAINING = 'Remaining';
 export default class MyNetworkCaseInvestigationMilestones extends LightningElement {
@@ -14,7 +22,6 @@ export default class MyNetworkCaseInvestigationMilestones extends LightningEleme
 	hasMilestonesViolated; //flag to check the milestones violated or not
 	isLoading = false; //flag to show/hide the spinner
 	milestoneDetails = {}; //milestone details
-	openViolation = 'Open Violation';
 
 	//get CI record details. 
 	@wire(getRecord, {
@@ -28,6 +35,7 @@ export default class MyNetworkCaseInvestigationMilestones extends LightningEleme
 		data
 	}) {
 		if (error) {
+			//set the error message
 			this.error = 'Error getting milestones';
 			if (Array.isArray(error.body)) {
 				this.error = error.body.map(e => e.message).join(', ');
@@ -48,6 +56,14 @@ export default class MyNetworkCaseInvestigationMilestones extends LightningEleme
 		}
 	}
 
+	renderedCallback() {
+		//set the progress bar width
+		if (this.template.querySelector('div.progress-bar') !== null) {
+			this.template.querySelector('div.progress-bar').style.width = this.milestoneDetails.percentCompleted + '%';
+		}
+	}
+
+	//load the milestone details of the case investigation
 	loadMilestones() {
 		if (this.caseInvestigation.NetworkMilestonesViolated__c !== null && this.caseInvestigation.NetworkMilestonesViolated__c !== undefined) {
 			this.hasMilestonesViolated = true;
@@ -57,6 +73,7 @@ export default class MyNetworkCaseInvestigationMilestones extends LightningEleme
 		}
 	}
 
+	//function to convert the mins to string to display it in the UI (ex: 1530 => 1d 1h 30m )
 	getMinutesToTimeString(timeInMins) {
 		let days = Math.floor(timeInMins / 1440);
 		let hours = Math.floor((timeInMins % 1440) / 60);
@@ -68,25 +85,28 @@ export default class MyNetworkCaseInvestigationMilestones extends LightningEleme
 		return timeString;
 	}
 
+	//function to sest the case investigation milestone to render on UI
 	getMilestoneDetails() {
 		if (this.caseInvestigation.NetworkMilestonesViolated__c <= 2) {
 			this.milestoneDetails.tierName = 'Network Tier ' + `${this.caseInvestigation.NetworkMilestonesViolated__c}`;
-			this.milestoneDetails.status = REMAINING;
 			this.milestoneDetails.timeRemainingOrCompleted = this.getMilestoneTimeRemaining();
 			this.milestoneDetails.percentCompleted = this.getPercentCompleted();
+			this.milestoneDetails.status = this.milestoneDetails.percentCompleted > 100 ? OPEN_VIOLATION : REMAINING;
 			this.milestoneDetails.progressBarClass = 'progress-bar ' + this.getProgressBarClass(this.milestoneDetails.percentCompleted);
 			this.milestoneDetails.iconName = 'glyphicon ' + this.getIconByMilestoneStatus(this.milestoneDetails.percentCompleted);
 		} else {
-			//default to tier 2 as we've only 2 network tiers for case investigations
+			// if milestones violated greater than 2 default to tier 2 as we've only 2 network tiers for case investigations
 			this.milestoneDetails.tierName = 'Network Tier 2';
-			this.milestoneDetails.status = OPEN_VIOLATION;
 			this.milestoneDetails.timeRemainingOrCompleted = this.getMilestoneTimeCompleted();
 			this.percentCompleted = 100;
+			this.milestoneDetails.status = OPEN_VIOLATION;
 			this.milestoneDetails.progressBarClass = 'progress-bar progress-bar-danger';
 			this.milestoneDetails.iconName = 'glyphicon glyphicon-thumbs-down';
 		}
 	}
 
+	//function to get the milestone time remaining for the tier
+	//calculates the remaining time from now to record next violation date time
 	getMilestoneTimeRemaining() {
 		let todaysDate = new Date();
 		let nextViolationDate = new Date(this.caseInvestigation.NetworkMilestoneNextViolationDatetime__c);
@@ -94,6 +114,8 @@ export default class MyNetworkCaseInvestigationMilestones extends LightningEleme
 		return this.getMinutesToTimeString(diffInMins);
 	}
 
+	//function to get the milestone time completed for the tier
+	//calculates the time completed from record last violation date time to now 
 	getMilestoneTimeCompleted() {
 		let todaysDate = new Date();
 		let lastViolationDate = new Date(this.caseInvestigation.NetworkMilestoneLastViolationDatetime__c);
@@ -101,6 +123,7 @@ export default class MyNetworkCaseInvestigationMilestones extends LightningEleme
 		return this.getMinutesToTimeString(diffInMins);
 	}
 
+	//function to get the percent of time completed to render for the progress bar
 	getPercentCompleted() {
 		let startDate;
 		let todaysDate = new Date();
@@ -108,11 +131,13 @@ export default class MyNetworkCaseInvestigationMilestones extends LightningEleme
 		if (this.caseInvestigation.NetworkMilestonesViolated__c === 1) {
 			startDate = new Date(this.caseInvestigation.MilestoneTimeStartDatetime__c);
 		} else {
+			//for network tier 2
 			startDate = new Date(this.caseInvestigation.NetworkMilestoneLastViolationDatetime__c);
 		}
 		return Math.round(((todaysDate - startDate) / (endDate - startDate)) * 100);
 	}
 
+	//function to get the progress bar class for UI based on the percent of time completed
 	getProgressBarClass(percent) {
 		let bgClass = 'progress-bar-success';
 		if ((percent >= 60 && percent < 80)) {
@@ -123,12 +148,15 @@ export default class MyNetworkCaseInvestigationMilestones extends LightningEleme
 		return bgClass;
 	}
 
+	//function to get the icon name for UI based on the percent of time completed
 	getIconByMilestoneStatus(percent) {
 		let iconName = '';
-		if (percent >= 60 && percent() < 80) {
+		if (percent >= 60 && percent < 80) {
 			iconName = 'glyphicon-eye-open';
-		} else if (percent >= 80) {
+		} else if (percent >= 80 && percent < 100) {
 			iconName = 'glyphicon-fire';
+		} else if (percent > 100) {
+			iconName = 'glyphicon-thumbs-down';
 		} else {
 			iconName = 'glyphicon-time';
 		}
