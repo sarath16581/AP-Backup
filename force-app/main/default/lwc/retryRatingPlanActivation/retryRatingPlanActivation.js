e69e7f59dd21c34a063bf152020dd2d9fd334055
Sarath Burra <sarath.burra@auspost.com.au>
2023-06-25 20:46:21 +1000

    CI-613 Dates Initial Check in

--- force-app/main/default/lwc/retryRatingPlanActivation/retryRatingPlanActivation.js
+++ force-app/main/default/lwc/retryRatingPlanActivation/retryRatingPlanActivation.js
@@ -1,17 +1,19 @@
 /*
-/* @author 
+/* @author
  * @date 2022-07-01
  * @group Billing Accounts
  * @tag Billing Account
- * @description: Rating Plan Activation Request Retry modal popup. 
+ * @description: Rating Plan Activation Request Retry modal popup.
  * 				 Used to display a prompt message and retry a callout to Camunda for SAP Integration.
  * @changelog
  * 2022-07-01 seth.heang@auspost.com.au Created
- */ 
+ * 2023-06-15 sarath.burra@auspost.com.au Added ConnectedCallBack to check if Service Commencement Date and Service Expiry Date have been populated
+ */
 import { LightningElement, api, wire, track } from 'lwc';
 import { CloseActionScreenEvent } from 'lightning/actions';
-import activateRatingPlan from "@salesforce/apex/CreateBillingAccountController.activateRatingPlan"; 
-import ratingPlanActivationCreationRequest from "@salesforce/apexContinuation/CreateBillingAccountController.ratingPlanActivationCreationRequest"; 
+import activateRatingPlan from "@salesforce/apex/CreateBillingAccountController.activateRatingPlan";
+import ratingPlanActivationCreationRequest from "@salesforce/apexContinuation/CreateBillingAccountController.ratingPlanActivationCreationRequest";
+import checkServiceDatesOnALI from "@salesforce/apex/CreateBillingAccountController.checkServiceDatesOnALI";
 import RATING_PLAN_INTEGRATION_STATUS from "@salesforce/schema/Deal_Support_Request__c.Rating_Integration_Status__c";
 import RATING_PLAN_INTEGRATION_NOT_CLEAR_ERROR_MSG from '@salesforce/label/c.RatingPlanIntegrationStatusNotClear';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
@@ -24,18 +26,19 @@ export default class RetryRatingPlanActivation extends LightningElement {
 	submitRequestComplete = false;
 	ratingPlanCreationSuccessful = false;
 	ratingPlanCalloutFailure = false;
+	serviceDatesError=false;
 	failedErrMsg = '<h3><b>There seems to be an issue while creating the rating plan activation request.<br/><br/>Please report an IT issue via the \'Report a Fault\' icon on the MyIT Service Portal and select the following details:</b><br/><br/>'+
 					'<ul><li>Please select the area this relates to: 2.Applications & Software<br/></li>'+
 					'<li>What does this relate to? Salesforce<br/></li>'+
 					'<li>What best describes your issue? Received message from Camunda stating that service is currently unavailable<br/></li>'+
 					'<li>Can you provide a short description:  SAP Rating Plan Activation integration is not working</li><ul></h3>';
 	integrationStatusIsNotCleared = null;
-	integrationStausRetryErrorMsg = RATING_PLAN_INTEGRATION_NOT_CLEAR_ERROR_MSG; 
+	integrationStausRetryErrorMsg = RATING_PLAN_INTEGRATION_NOT_CLEAR_ERROR_MSG;
 	passIntegrationStatusValidation = false;
 
 	/**
 	 * @description	wire method to retrieve the DSR record and rating plan integration status field, to be used for validation
-	 *  */ 
+	 *  */
 	@wire(getRecord, { recordId: '$recordId', fields: [RATING_PLAN_INTEGRATION_STATUS] })
 	wiredRatePlanIntegrationStatus({error, data}){
 		if(error){
@@ -56,6 +59,16 @@ export default class RetryRatingPlanActivation extends LightningElement {
 		}
 	}
 
+	@api
+    connectedCallback(event){
+        checkServiceDatesOnALI({ dsrId: this.recordId})
+        .then(result => {
+            this.serviceDatesError = result;
+        })
+        .catch(error => {
+            console.log('Error is: '+error);
+        });
+    }
 	/**
 	 * @description	this fuction imperatively call apex methods to generate a request payload with all required mappings, create BAM external onboarding request
 	 * 				and finally trigger a callout to Camunda
