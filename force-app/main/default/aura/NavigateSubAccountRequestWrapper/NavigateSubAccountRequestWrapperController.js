cba6d7825f2e6f05d27e0a0207569ae339b1f9c7
Harry Wang <harry.wang23@auspost.com.au>
2023-08-21 17:51:02 +1000

    STP-9204 Initial commit

---
+++ force-app/main/default/aura/NavigateSubAccountRequestWrapper/NavigateSubAccountRequestWrapperController.js
@@ -0,0 +1,16 @@
+/*
+  * @author       : Harry Wang
+  * @date         : 2023-08-18
+  * @description  : Component used as wrapper for Sub Account Request Navigation from Billing Account.
+  *                 Either createTEAMFollowerOffspringRequest or createSubAccountsRequest is opened based on the value of isTeamRequest
+  @changelog
+  2023-08-18    Harry Wang - Created
+*/
+({
+    doInit: function(component, event, helper) {
+	    const pageReference = component.get("v.pageReference");
+	    component.set("v.recordId", pageReference.state.c__recordId);
+	    component.set("v.initialLoad", pageReference.state.c__initialLoad);
+	    component.set("v.isTEAMRequest", pageReference.state.c__isTEAMRequest);
+    }
+});
