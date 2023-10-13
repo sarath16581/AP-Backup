aae3d2223c059c783dca967c0aeb926b0ff0cbd1
Thang Nguyen <thangnt75@gmail.com>
2023-10-13 11:49:19 +1100

    Remove further spaces

--- force-app/main/default/lwc/bspUploadFiles/bspUploadFiles.js
+++ force-app/main/default/lwc/bspUploadFiles/bspUploadFiles.js
@@ -1,9 +1,9 @@
 /*
-* @author       : Jansi Rani. jansi.rani@auspost.com.au
-* @date         : 21/08/2020
-* @description  : Component for Uploading files.
-* @changes      :
-*     23/08/2023 Hasantha Liyanage : allow by pass accepted file formats
+* @author		: Jansi Rani. jansi.rani@auspost.com.au
+* @date			: 21/08/2020
+* @description	: Component for Uploading files.
+* @changes		:
+*				23/08/2023 Hasantha Liyanage : allow by pass accepted file formats
 */
 import { LightningElement, track, api } from 'lwc';
 import userId from '@salesforce/user/Id';
