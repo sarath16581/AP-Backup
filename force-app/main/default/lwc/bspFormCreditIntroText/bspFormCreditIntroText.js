d66e2e11a453cecd1bcc0f2342d4527142bb539a
Hasantha Liyanage <hasanthaera@gmail.com>
2023-11-06 09:44:06 +1100

    SB: info text header

---
+++ force-app/main/default/lwc/bspFormCreditIntroText/bspFormCreditIntroText.js
@@ -0,0 +1,37 @@
+/**
+ * 	@author Hasantha Liyanage
+ * 	@date 2023-11-02
+ * 	@group child component
+ * 	@domain BSP
+ * 	@description info text on bsp credit claim header
+ * 	@changelog
+ * 	2023-11-02 - Hasantha Liyanage  - Created
+ */
+
+import {api, LightningElement} from 'lwc';
+
+export default class BspFormCreditIntroText extends LightningElement {
+
+	@api billingAccountScope;
+
+	get isAp() {
+		if (this.billingAccountScope == 'AP')
+			return true;
+		else
+			return false;
+	}
+
+	get isSt() {
+		if (this.billingAccountScope == 'ST')
+			return true;
+		else
+			return false;
+	}
+
+	get isAll() {
+		if (this.billingAccountScope == 'ALL')
+			return true;
+		else
+			return false;
+	}
+}
