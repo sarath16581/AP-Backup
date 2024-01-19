784ce73597f5bedde5a2924e1981487743fe91e5
stephenliu8964 <dq89_64@hotmail.com>
2024-01-19 12:35:25 +1100

    account offboarding close account button
    - refactored the lwc to use an apex controller
    - modified the lwc to fetch the base url from a custom setting
    - added a custom setting to contain the base url

--- force-app/main/default/lwc/closeAccount/closeAccount.js
+++ force-app/main/default/lwc/closeAccount/closeAccount.js
@@ -1,25 +1,22 @@
 import { LightningElement,api,wire } from 'lwc';
-import { getRecord, getFieldValue } from "lightning/uiRecordApi";
 import { NavigationMixin } from 'lightning/navigation';
-import APCN_FIELD from "@salesforce/schema/Account.APCN__c";
+import getBaseUrl from '@salesforce/apex/CloseAccountController.getBaseUrl';
+import getAPCN from '@salesforce/apex/CloseAccountController.getAPCN';
 
-const fields = [APCN_FIELD];
-const baseUrl = 'http://hxaix23.hq.auspost.com.au:50000/irj/go/km/docs/SAP_AusPost_Web_Content/Enterprise/Display/PortalUniqueRedirectSecure.html?URL=sapfes.hq.auspost.com.au/sap/bc/ui5_ui5/sap/zcs_cac/index.html#/apcn/';
 export default class CloseAccount extends NavigationMixin(LightningElement) {
-    @api recordId
+	@api recordId
+	baseUrl;
+	apcn;
 
-    @wire(getRecord, { recordId:'$recordId', fields})
-    loadFields({error, data}){
-        if(data){
-            const apcn = getFieldValue(data, APCN_FIELD);
+	async connectedCallback() {
+		this.baseUrl = await getBaseUrl();
+		this.apcn = await getAPCN({accId: this.recordId});
 
-            let url = baseUrl + apcn;
-            this[NavigationMixin.Navigate]({
-                type: 'standard__webPage',
-                attributes: {
-                    url: url
-                }
-            });
-        }
-    }
+		this[NavigationMixin.Navigate]({
+		type: 'standard__webPage',
+			attributes: {
+				url: this.baseUrl + this.apcn
+			}
+		});
+	}
 }
