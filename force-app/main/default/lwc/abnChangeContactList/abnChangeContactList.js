64228d533f1ff33478158d2f36abae9ea5c3b4a2
Harry Wang <harry.wang23@auspost.com.au>
2024-05-24 12:33:49 +1000

    STP-10427 Adding tests and comments

---
+++ force-app/main/default/lwc/abnChangeContactList/abnChangeContactList.js
@@ -0,0 +1,16 @@
+/**
+ * @description Custom Datatable extends standard datatable to have control of scrolling
+ * @author Harry Wang
+ * @date 2024-05-17
+ * @changelog
+ * 2024-05-17 - Harry Wang - Created
+ */
+import {api} from 'lwc';
+import LightningDatatable from "lightning/datatable";
+
+export default class AbnChangeContactList extends  LightningDatatable {
+	@api
+	scrollToTop() {
+		this.template.querySelector(".slds-scrollable_y").scrollTop = 0;
+	}
+}
