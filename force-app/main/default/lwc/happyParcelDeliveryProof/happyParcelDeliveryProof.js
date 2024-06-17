209b9b7cde0bca529b463742abe11e597cd11055
Seth Heang <panhaseth.heang@auspost.com.au>
2024-06-17 11:05:50 +1000

    cleanup

--- force-app/main/default/lwc/happyParcelDeliveryProof/happyParcelDeliveryProof.js
+++ force-app/main/default/lwc/happyParcelDeliveryProof/happyParcelDeliveryProof.js
@@ -23,10 +23,7 @@ import {
 	subscribe,
 	unsubscribe,
 	publish,
-	downloadDeliveryProofPdf,
-	getSafeDropImageStateForDownload,
-	getSafeDropImageAndSaveForPOD,
-	getTrackingApiResponse, getTrackingApiResponseForStarTrack
+	downloadPODPDF
 } from 'c/happyParcelService';
 import HappyParcelBase from "c/happyParcelBase";
 
