6b21b591869e17ead1eb3dae8a810961e2bb4901
Paul Perry <paul.perry5@auspost.com.au>
2024-07-24 15:47:20 +1000

    CTI Uplift

---
+++ force-app/main/default/staticresources/GenesysConnector/integration.js
@@ -0,0 +1,196 @@
+/**
+ * @description Genesys CTI Integration. This logic hooks up all the cti (and cti related) events
+ *  and forward these to either the AP or ST connector, based on the interaction's ccDivision attribute value
+ * @author Paul Perry
+ * @date 2024-04-26
+ * @changelog
+ *
+ */
+	console.log('Genesys Connector Widget Integration: init');
+
+	// Properties used for each interaction
+	let businessLogic;
+	let lastCtiInteractionLog = { };
+	let ccDivision; // Either AP or ST
+
+	// Properties used for testing and tracking
+	let eventTracking = { };
+	let mockCtiParams = { };
+	let eventList = [ ];
+
+	addCustomAttributesMethod = (id, attributes, callback) => {
+		sforce.console.fireEvent(
+			'inin.salesforce.constants.consoleevent.addCustomAttributes',
+			JSON.stringify({ data: { id, attributes } }),
+			callback ? callback : (data) => console.log(data)
+		);
+	}
+
+	// create map for console interaction methods
+	// - https://developer.salesforce.com/docs/atlas.en-us.api_console.meta/api_console/sforce_api_console_methods_tabs.htm
+	const consoleMethods = {
+		// tab related methods
+		// Please refer to the documentation in order to provide the correct parameters for each console interaction method:
+		closeTab : sforce.console.closeTab,
+		disableTabClose : sforce.console.disableTabClose,
+		focusPrimaryTabById : sforce.console.focusPrimaryTabById,
+		generateConsoleUrl : sforce.console.generateConsoleUrl,
+
+		// These don't work as this logic doesn't sit on the tab itself...
+		getEnclosingPrimaryTabId : sforce.console.getEnclosingPrimaryTabId,
+		getFocusedPrimaryTabId : sforce.console.getFocusedPrimaryTabId,
+		getFocusedPrimaryTabObjectId : sforce.console.getFocusedPrimaryTabObjectId,
+		getFocusedSubtabId : sforce.console.getFocusedSubtabId,
+		getFocusedSubtabObjectId : sforce.console.getFocusedSubtabObjectId,
+		getPageInfo : sforce.console.getPageInfo,
+		getPrimaryTabIds : sforce.console.getPrimaryTabIds,
+		openPrimaryTab : sforce.console.openPrimaryTab,
+		getTabLink : sforce.console.getTabLink,
+		// custom attributes -> update call interaction details
+		setCustomAttributes : this.addCustomAttributesMethod
+	}
+
+	// Invoke a console event from the Business logic implementation class
+	invokeConsoleEvent = (methodName, ...params) => {
+		if (!consoleMethods[methodName]) {
+			throw new Error(`Method not implememnted: ${methodName}`);
+		}
+
+		return consoleMethods[methodName](...params);
+	}
+
+	connectAllCtiEvents = () => {
+		// Always turn off mocking onload:
+		// GenesysCTIUtils.mockActive = false;
+
+		const ALL_EVENTS = {
+			// ALL CTI Events:
+			'INTERACTION_CONNECTED' : 'inin.salesforce.constants.consoleevent.pc.INTERACTION_CONNECTED',
+			'INTERACTION_DISCONNECTED' : 'inin.salesforce.constants.consoleevent.pc.INTERACTION_DISCONNECTED',
+			'LOGGED_OUT' : 'inin.salesforce.constants.consoleevent.pc.LOGGED_OUT',
+			'ACW_REQUIRED' : 'inin.salesforce.constants.consoleevent.pc.ACW_REQUIRED',
+			'ACW_COMPLETED' : 'inin.salesforce.constants.consoleevent.pc.ACW_COMPLETED',
+			'INTERACTION_EVENT' : 'inin.salesforce.constants.consoleevent.pc.INTERACTION_EVENT'
+		};
+
+		// Wireup all event handlers to CTI EventListener
+		Object.keys(ALL_EVENTS).forEach(eventName => sforce.console.addEventListener(
+			ALL_EVENTS[eventName], (event) => handleCtiEvent(eventName, event)
+		));
+
+		// Wireup tab focus change
+		sforce.console.onFocusedPrimaryTab((event) => handleCtiEvent('PRIMTABFOCUS_CHANGE', event));
+		sforce.console.onFocusedSubtab((event) => handleCtiEvent('SUBTABFOCUS_CHANGE', event));
+
+		// Wire up storage event to CTI Eventlistener
+		// - Storage events are used for cross window / iframe communication
+		window.addEventListener('storage', (event) => {
+			if (event.key.startsWith('CTI_')) {
+				// Provide event detail newValue and oldValue
+				const detail = ['newValue', 'oldValue'].reduce(
+					(res, item) => Object.assign(res, { [item] : GenesysCTIUtils.jsonToObj(event[item]) }), { }
+				);
+				// Provide array of changed attributes
+				detail.changes = GenesysCTIUtils.getDiff(detail.newValue, detail.oldValue);
+				handleCtiEvent('STORAGE', { detail, key : event.key });
+			}
+		});
+
+		// Clearview codes
+		['Complaint__c', 'Type', 'ProductCategory__c', 'ProductSubCategory__c', 'EnquirySubType__c'].forEach(
+			field => sforce.console.addEventListener(
+				`MiniCaseFieldSet_${field}`, (event) => handleCtiEvent('CLEARVIEWCODES', { field, event })
+			)
+		);
+
+		sforce.console.addEventListener('CaseDetails_Refreshed', (caseId) => {
+			handleCtiEvent('CASEUPDATE', caseId)
+		});
+
+		sforce.console.addEventListener('genesys.connector.mockevent', (event) => {
+			const ctiEvent = JSON.parse(event.message);
+			handleCtiEvent(ctiEvent.eventName, ctiEvent.message);
+		});
+	}
+
+	// Which call centre logic do we need to handle calls? (AP / ST)
+	// The below solution is based on the idea that Genesys provides an attribute called
+	// Participant.Division that would drive this logic to pick the correct handler class
+	initialiseCallCentreHandlerInstance = (ctiEvent) => {
+		this.ccDivision = ctiEvent.detail?.attributes?.['Participant.Division'];
+
+		if (!this.ccDivision && ctiEvent.detail?.['queueName']?.startsWith('CS_')) {
+			this.ccDivision = 'AP';
+		}
+
+		if (this.ccDivision === 'ST') {
+			return new GenSTBusinessLogic();
+		} else if (this.ccDivision === 'AP') {
+			return new GenesysAPBusinessLogic(invokeConsoleEvent);
+		} else {
+			throw Error(`Missing/unknown value for Participant.Division attribute: "${this.ccDivision}"`);
+		}
+	}
+
+	// Returns milliseconds since last invocation, used to track operation time
+	timerLapse = () => {
+		const stamp = new Date().getTime();
+
+		if (!this._timer) {
+			this._timer = stamp;
+			return 0;
+		} else {
+			try {
+				return stamp - this._timer;
+			} finally {
+				this._timer = stamp;
+			}
+		}
+	}
+
+	handleCtiEvent = (eventName, event) => {
+		console.log(eventName, event);
+		const result = GenesysCTIUtils.extractEventDetail(eventName, event);
+
+		if (eventName === 'INTERACTION_EVENT') {
+			// merge mocked interaction properties, if provided
+			if (GenesysCTIUtils.mockActive) {
+				result.detail = GenesysCTIUtils.deepCloneObj(result.detail, GenesysCTIUtils.mockParams);
+			}
+
+			lastCtiInteractionLog = result.detail;
+
+			// TODO: Decide whether we need this on every call, or only the first one:
+			if (result.category === 'add' /*&& !businessLogic*/) {
+				// first CTI event for inbound call (category 'add') means wiring up the division specific CTI adapter logic
+				businessLogic = initialiseCallCentreHandlerInstance(result);
+			}
+		} else if (eventName !== 'INTERACTION_EVENT') {
+			if (eventName === 'INTERACTION_DISCONNECTED' && lastCtiInteractionLog) {
+				// Stop incoming minicase updates from being processed when disconnected
+				lastCtiInteractionLog.isConnected = false;
+				lastCtiInteractionLog.state = 'DISCONNECTED';
+			}
+
+			// In case the event isn't an InteractionEvent containing the current Call Interaction details,
+			// provide the last known object
+			result.lastDetail = lastCtiInteractionLog;
+		}
+
+		// For tracking purposes only
+		const trackedEvents = ['INTERACTION_CONNECTED', 'INTERACTION_DISCONNECTED', 'LOGGED_OUT', 'ACW_REQUIRED', 'ACW_COMPLETED', 'INTERACTION_EVENT'];
+		if (trackedEvents.includes(eventName)) {
+			eventList.push({
+				...result,
+				timeSinceLast : timerLapse()
+			});
+		}
+
+		sforce.console.fireEvent('genesys.connector.trackevent', JSON.stringify({ eventName, ctiEvent : result }));
+
+		// Pass the event onto the instance that handles the business logic
+		if (businessLogic && (eventName !== 'INTERACTION_EVENT' || (result.category === 'add' || result.changes?.length))) {
+			// Only invoke the handler if any cti attributes have actually changed or if not an interaction event
+			businessLogic.handleCtiEvent(eventName, result);
+		}
+	}
---
+++ force-app/main/default/staticresources/GenesysConnector/starTrackConnector.js
@@ -0,0 +1,316 @@
+/**********************************************************
+ * @ Vasu Gorakati - 16-04-2024 Initial version CTI Uplift project
+ **********************************************************/
+
+const consignmentSearchTabName = 'ConsignmentSearch';
+const consignmentSearchUrl = '/apex/StarTrackConsignmentSearch';
+const logPrefix = 'StarTrackLog ';
+
+class GenSTBusinessLogic {
+    callLog;
+	stFieldMappings = {
+        enquiryType: 'attributes.Participant\\.Enquiry_Type',
+        consignmentNumber: 'attributes.Participant\\.Consignment',
+        phoneNumber: 'ani',
+        customerSegment: 'attributes.Participant\\.Customer_Segment',
+        serviceType: 'attributes.Participant\\.Service_Type',
+        serviceSubType: 'attributes.Participant\\.Service_Subtype',
+        atlFlag: 'attributes.Participant\\.ATL',
+        exitCode: 'attributes.Participant\\.Exit_Code',
+        partyType: 'attributes.Participant\\.Contact_Type'
+    };
+
+    handleCtiEvent(eventName, eventDetail) {
+        console.log(logPrefix + 'eventName ' + eventName);
+        console.log(logPrefix + 'eventDetail ' + eventDetail);
+
+        if (eventName === 'INTERACTION_EVENT') {
+            this.callLog = new CallInteractionProxy(eventDetail.detail, this.stFieldMappings);
+			if (this.callLog.phoneNumber?.startsWith('+61')) {
+				this.callLog.phoneNumber = `0${this.callLog.phoneNumber.slice(3)}`;
+			}
+        }
+
+        if (eventName === 'INTERACTION_CONNECTED' && eventDetail.lastDetail.direction === 'Inbound') {
+            this.handleSTLogic();
+        }
+    }
+
+    handleSTLogic() {
+        console.log(logPrefix + 'call log==>' + JSON.stringify(this.callLog));
+        const {
+            enquiryType,
+            consignmentNumber,
+            customerSegment,
+            serviceType,
+            serviceSubType,
+            atlFlag,
+            exitCode,
+            partyType
+        } = this.callLog;
+		let { phoneNumber } = this.callLog;
+
+        let caseType = '';
+        let casePurpose = '';
+        let trackingSearchConsignment = false;
+        let cardIVRSearchCase = false;
+        let screenPopNoData = false;
+        let trackingSearchCase = false;
+
+        console.log(logPrefix + "enquiryType ==>" + enquiryType);
+        console.log(logPrefix + "consignmentNumber ==>" + consignmentNumber);
+        console.log(logPrefix + "phoneNumber ==>" + phoneNumber);
+        console.log(logPrefix + "customerSegment ==>" + customerSegment);
+        console.log(logPrefix + "serviceType ==>" + serviceType);
+        console.log(logPrefix + "serviceSubType ==>" + serviceSubType);
+        console.log(logPrefix + "atlFlag ==>" + atlFlag);
+        console.log(logPrefix + "exitCode ==>" + exitCode);
+
+        // Logic for Screen pops
+        if (enquiryType === "Priority" && (consignmentNumber === null || consignmentNumber === undefined)
+            && (customerSegment === "Priority 1" || customerSegment === "Priority 2")
+            && (serviceSubType === "Invalid or No Selection" || serviceSubType === "Tracking Enquiry")
+            && serviceType === "Main Menu") {
+            console.log(logPrefix + "Scenario: Tracking - Screen Pop without data.");
+            screenPopNoData = true; // Flag to Pop out the search consignment page without data
+        } else if (enquiryType === "BusinessSolutions") {
+            if (consignmentNumber === null || consignmentNumber === undefined) {
+                console.log(logPrefix + "Scenario: BusinessSolutions - Screen Pop without data.");
+                screenPopNoData = true; // Flag to Pop out the search consignment page with data
+            } else {
+                console.log(logPrefix + "Scenario: BusinessSolutions - Screen Pop with data.");
+                trackingSearchConsignment = true; // Flag to Pop out the search consignment page with data
+            }
+        } else if (enquiryType === "Tracking" && serviceType === "Tracking and Cards" && customerSegment === "Main") {
+            console.log(logPrefix + "Scenario: Tracking|Tracking and Cards");
+            if ((exitCode === "Error" || exitCode === "Duplicate") && (serviceSubType === "Error" || serviceSubType === "Duplicate Consignment Found" || serviceSubType === "Duplicate Consignments Found")) {
+                trackingSearchConsignment = true; // Flag to Pop out the search consignment page with data
+                console.log(logPrefix + "Scenario: Error| Duplicate");
+            } else if ((exitCode === "NoScan" || exitCode === "TransferRequired" || exitCode === "Transit")) { // Removed as hopefully not needed! && (serviceSubType === "No Scanning Events" || serviceSubType === "In Transit or Delivered")){
+                trackingSearchCase = true; // Flag to search for the related case on a consignment
+                console.log(logPrefix + "Scenario: NoScan|Transit|TransferRequired");
+            } else if ((exitCode === "Redeliver" || exitCode === "Redirect" || exitCode === "Depot") && (serviceSubType === "Organise Redelivery" || serviceSubType === "Organise Redirection" || serviceSubType === "Depot Collection")) {
+                cardIVRSearchCase = true; // Flag to search for the related case on a consignment
+                console.log(logPrefix + "Scenario: Redeliver|Redirect|Depot");
+
+                // Based on the exit code, any case that is created should be created with a specific case type
+                casePurpose = 'Card Left';
+                if (exitCode === "Redeliver") {
+                    caseType = 'Redelivery';
+                } else if (exitCode === "Redirect") {
+                    caseType = 'Redirection';
+                } else if (exitCode === "Depot") {
+                    caseType = 'Depot Collection';
+                }
+            } else {
+                console.log(logPrefix + "Scenario: Tracking|Tracking and Cards| Main - No conditions met.");
+            }
+        } else if (enquiryType === "Tracking" && serviceType === "Main Menu" && customerSegment === "Training") {
+            trackingSearchConsignment = true; // Flag to Pop out the search consignment page with data
+            console.log(logPrefix + "Scenario: Training");
+        } else {
+            console.log(logPrefix + "Selection is not valid on any scenario");
+        }
+
+        if (screenPopNoData) {
+            this.popConsignmentSearchpage(null, null);
+        } else if (trackingSearchConsignment) {
+            this.popConsignmentSearchpage(consignmentNumber, partyType);
+        } else if (trackingSearchCase) {
+            this.searchCaseUsingConsignment(consignmentNumber, phoneNumber, atlFlag, casePurpose, caseType, partyType);
+        } else if (cardIVRSearchCase) {
+            this.searchCaseUsingConsignment(consignmentNumber, phoneNumber, atlFlag, casePurpose, caseType, partyType);
+        }
+    }
+
+    popConsignmentSearchpage(consignment, partyType) {
+        console.log(logPrefix + "popConsignmentSearchpage");
+
+        if (sforce.console.isInConsole()) {
+            // Attempt to refresh the primary consignment search tab
+            // If it fails, it's not open and we attempt to open it as a new tab
+            sforce.console.refreshPrimaryTabByName(consignmentSearchTabName, true, function (result) {
+                const openCompleteCallback = function () {
+                    if (consignment != null && consignment != '' && consignment !== false) {
+                        console.log(logPrefix, 'consignment search opened', consignment)
+                        // Fire the event to set the consignment search string and trigger a search.
+                        // This is picked up by StarTrack Consignment Search page.
+                        // Also send the caller type which is either Sender / Receiver based on the response they gave in the IVR
+                        const listener = function (result) {
+                            console.log(logPrefix, 'RequestParentData_SearchString received');
+                            let datapop = { 'consignment': consignment, 'contactType': '' };
+                            if (partyType != null && partyType != '') {
+                                datapop = { 'consignment': consignment, 'contactType': partyType };
+                            }
+                            sforce.console.fireEvent('ParentDataResponse_SearchString', JSON.stringify(datapop));
+                            sforce.console.removeEventListener('RequestParentData_SearchString', listener);
+                        };
+                        sforce.console.addEventListener('RequestParentData_SearchString', listener);
+                    }
+                };
+
+                if (!result.success) {
+                    // The window could not be focused so that generally means it doesn't exist.
+                    // Attempt to open up a window
+                    sforce.console.openPrimaryTab(null, consignmentSearchUrl, true, 'Consignment Search', openCompleteCallback, consignmentSearchTabName);
+                } else {
+                    if (consignment != null && consignment != '' && consignment !== false) {
+                        sforce.console.fireEvent('ParentDataResponse_SearchString', consignment);
+                    }
+                }
+            });
+        }
+    }
+
+    // Method called for searching consignment number via searchconsignment page
+    searchCaseUsingConsignment(consignmentNumber, phoneNumber, atlFlag, casePurpose, caseType, contactType) {
+        console.log(logPrefix + "searchCaseUsingConsignment" + consignmentNumber);
+
+		new Promise(callback => GenesysConnectorController.findConsignmentStarTrack(consignmentNumber, callback))
+		.then((result ) =>{
+                console.log(logPrefix, 'findConsignment result', result);
+
+                const res = result.split("_");
+
+                if (result.includes("NoCase")) { // No case exists against Consignment, Create a case
+                    this.createCaseForConsignment(res[1], phoneNumber, atlFlag, casePurpose, caseType, contactType);
+                } else if (result.includes("MultipleCase")) {
+                    // Pop completed consignment search page
+                    // This also includes a scenario where the consignment doesn't exist in Salesforce yet too.
+                    this.popConsignmentSearchpage(consignmentNumber, contactType);
+                } else {
+                    // If only one case is related to a consignment
+                    if (!result.includes("Closed")) {
+                        // Make sure Case is not Closed
+                        // Check related contacts of case
+                        this.checkRelatedContactOfCase(res[0], res[1], phoneNumber, contactType);
+                    } else if (result != '') {
+                        this.openCaseRecord(res[0], res[1], false); // Open closed case
+                    }
+                }
+            }
+        ).catch(
+			function (err) { console.error(err); }
+		);
+    }
+
+
+    // Method to check related contacts on a case
+    checkRelatedContactOfCase(caseId, caseNumber, phoneNumber, callerType) {
+        console.log(logPrefix + "checkRelatedContactOfCase");
+
+        new Promise(callback => GenesysConnectorController.checkRelatedContactOfCaseStarTrack(caseId, phoneNumber, callback))
+         .then((result) => {
+                if (result !== null) {
+                    this.updateCaseWithRelatedContact(result, caseId);
+                } else {
+                    // Pop up the case and add logic for prepopulating contact side panel in edit mode
+                    this.openCaseRecord(caseId, caseNumber, false);
+                    this.prepopulateContactSidePanel(phoneNumber, callerType, caseId);
+                }
+            }
+        ).catch(
+			function (err) { console.error(err); }
+		);
+    }
+
+    // Method to update case with new related contact and pops out the case
+    updateCaseWithRelatedContact(relatedContact, caseId) {
+        console.log(logPrefix + "updateCaseWithRelatedContact");
+
+        new Promise(callback =>GenesysConnectorController.updateCaseWithRecentCallerStarTrack(relatedContact, caseId, callback))
+        .then((result) => {
+                if (result !== null) {
+                    // Pop up the case and loads related contact on contact side panel in edit mode
+                    this.openCaseRecord(result.Id, result.CaseNumber, true);
+                    this.loadRelatedContact(relatedContact.Id, result.Id);
+                }
+            }
+        ).catch(
+			function (err) { console.error(err); }
+		);
+    }
+
+    // Create case for consignment
+    createCaseForConsignment(consignmentNumber, phoneNumber, atlFlag, casePurpose, caseType, contactType) {
+        console.log(logPrefix + "createCaseForConsignment");
+
+        // Logic here to create a case against a consignment
+        new Promise(callback =>GenesysConnectorController.createCasewithConsignmentNumberStarTrack(consignmentNumber, phoneNumber, atlFlag, casePurpose, caseType, contactType, callback))
+		.then((result) => {
+                if (result !== null) {
+                    // Pop up the case and add logic for prepopulating contact side panel in edit mode
+                    this.openCaseRecord(result.Id, result.CaseNumber, false);
+                    this.prepopulateContactSidePanel(phoneNumber, contactType, result.Id);
+                }
+            }
+        ).catch(
+			function (err) { console.error(err); }
+		);
+    }
+
+    // Pops out the case related to a consignment
+    openCaseRecord(caseId, caseNumber, callLog) {
+        console.log(logPrefix + "openCaseRecord", caseId, caseNumber, callLog);
+        // Open a new primary tab with the Salesforce.com home page in it
+        if (caseId != null) {
+            if (sforce.console.isInConsole()) {
+                sforce.console.openPrimaryTab(null, '/' + caseId, true, caseNumber, 'caseTab');
+            }
+
+            if (callLog) {
+                this.createCallLog(caseId);
+            }
+        }
+    }
+
+    // Creates call log of case
+    createCallLog(caseId) {
+        console.log(logPrefix + "createCallLog");
+
+        if (caseId != null) {
+            new Promise(callback =>GenesysConnectorController.createCallLogStarTrack(caseId, callback))
+			.then((result) => {
+                    console.log("openCaseRecord_createCallLog" + result);
+                }
+			).catch(
+				function (err) { console.error(err); }
+			);
+        }
+    }
+
+    // Fires event for the prepopulation of contact fields
+    prepopulateContactSidePanel(phoneNumber, callerType, recordId) {
+        console.log(logPrefix + 'prepopulateContactSidePanel');
+        if (sforce.console.isInConsole()) {
+            const listener = function (result) {
+                let payload = { phoneNumber, callerType: callerType, caseId: recordId };
+                payload = JSON.stringify(payload);
+                sforce.console.fireEvent('ParentDataResponse_AddContactDetails', payload);
+                sforce.console.removeEventListener('RequestParentData_AddContactDetails', listener);
+            };
+
+            // This may be problematic if the case is already open because the RequestParentData_AddContactDetails even will not fire
+            // It's currently fired by StarTrackAddContactSidePanel only after the page is loaded
+            sforce.console.addEventListener('RequestParentData_AddContactDetails', listener);
+        }
+    }
+
+    // Fires event for the prepopulation of related contact data
+    loadRelatedContact(relatedContactId, caseId) {
+        console.log(logPrefix + 'loadRelatedContact');
+        if (sforce.console.isInConsole()) {
+            const listener = function (result) {
+                let payload = { relatedContactId, caseId: caseId };
+                payload = JSON.stringify(payload);
+
+                sforce.console.fireEvent('ParentDataResponse_LoadContact', payload);
+                sforce.console.removeEventListener('RequestParentData_LoadContact', listener);
+            };
+
+            // This may be problematic if the case is already open because the RequestParentData_LoadContact even will not fire
+            // It's currently fired by StarTrackAddContactSidePanel only after the page is loaded
+            sforce.console.addEventListener('RequestParentData_LoadContact', listener);
+        }
+    }
+}
