89d204088fca5fd612d345a11ff5435b6918de10
bitbucket-pipelines <commits-noreply@bitbucket.org>
2024-08-21 04:28:59 +0000

    Changes from 2024-08-21

--- force-app/main/default/aura/auraOpportunityClosure/auraOpportunityClosureHelper.js
+++ force-app/main/default/aura/auraOpportunityClosure/auraOpportunityClosureHelper.js
@@ -1,15 +1,5 @@
 ({
-	showMyToast: function(component, helper, type, message) {
-		var toastEvent = $A.get("e.force:showToast");
-		var mode = 'dismissible';
-		if (type == 'error' || type == 'warning') {
-			mode = 'sticky';
-		}
-		toastEvent.setParams({
-			type: type,
-			mode: mode,
-			message: message
-		});
-		toastEvent.fire();
+	helperMethod : function() {
+		
 	}
 })
