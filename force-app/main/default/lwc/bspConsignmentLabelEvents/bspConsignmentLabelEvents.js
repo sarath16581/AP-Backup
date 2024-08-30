05cc52ad11c77887d0742a2f18886a39807c74d7
bitbucket-pipelines <commits-noreply@bitbucket.org>
2024-08-30 10:13:09 +0000

    Changes from 2024-08-30

--- force-app/main/default/lwc/bspConsignmentLabelEvents/bspConsignmentLabelEvents.js
+++ force-app/main/default/lwc/bspConsignmentLabelEvents/bspConsignmentLabelEvents.js
@@ -1,14 +1,28 @@
+/**
+ * @description LWC for displaying article event table in article's tracking Id search result for BSP Community
+ * @changelog:
+ * 2024-08-02 - Seth Heang - added passSafeDropDownloadState attribute pass-through
+*/
 import { LightningElement, api } from 'lwc';
 
 export default class BspConsignmentLabelEvents extends LightningElement {
-    @api labelEvents;
-    @api selectedEventArticle;
-    @api selectedConsignmentSearchType;
-    @api isConsignmentAuthenticated;
-    @api isConsignmentSerchIsAPType;
+	@api labelEvents;
+	@api selectedEventArticle;
+	@api selectedConsignmentSearchType;
+	@api isConsignmentAuthenticated;
+	@api isConsignmentSerchIsAPType;
+    
 
-    onChangeOfSelectedEvent(event){
-        const c = new CustomEvent('selectedarticlechange', {detail : event.detail});
-        this.dispatchEvent(c);
+	onChangeOfSelectedEvent(event){
+		const c = new CustomEvent('selectedarticlechange', {detail : event.detail});
+		this.dispatchEvent(c);
     }
+
+	@api 
+	setSafeDropDownloadLoading(isLoading) {
+		this.template.querySelectorAll("c-bsp-label-event-row").forEach((item) => {
+			item.setSafeDropDownloadLoading(isLoading);
+		});
+		  
+	}
 }
