ab50da617c6cecca22aefbbac6ac192341cbc44f
StephenL <stephen.liu25@auspost.com.au>
2024-09-09 11:24:06 +1000

    added comments and cleaning up

--- force-app/main/default/lwc/unifiedVoiceCallFieldSync/unifiedVoiceCallFieldSync.js
+++ force-app/main/default/lwc/unifiedVoiceCallFieldSync/unifiedVoiceCallFieldSync.js
@@ -11,13 +11,21 @@ import CALL_STATUS_FIELD from '@salesforce/schema/VoiceCall.CallDisposition';
 import CONSIGNMENT_FIELD from '@salesforce/schema/VoiceCall.Consignment__c';
 import TRACKING_NUMBER_FIELD from '@salesforce/schema/VoiceCall.TrackingNumber__c';
 import RELATED_RECORD_ID_FIELD from '@salesforce/schema/VoiceCall.RelatedRecordId';
-const VOICE_CALL_FIELDS = [RELATED_RECORD_FIELD, PREVIOUS_CALL_ID_FIELD, DIVISION_FIELD, NEXT_CALL_ID_FIELD, CALL_STATUS_FIELD, CONSIGNMENT_FIELD, TRACKING_NUMBER_FIELD, RELATED_RECORD_ID_FIELD];
+import PRODUCT_CATEGORY_FIELD from '@salesforce/schema/VoiceCall.ProductCategory__c';
+import PRODUCT_SUB_CATEGORY_FIELD from '@salesforce/schema/VoiceCall.ProductSubCategory__c';
+import CONTACT_FIELD from '@salesforce/schema/VoiceCall.Contact__c';
+import ENQUIRY_TYPE_FIELD from '@salesforce/schema/VoiceCall.EnquiryType__c';
+import ENQUIRY_SUB_TYPE_FIELD from '@salesforce/schema/VoiceCall.EnquirySubType__c';
+import ROOT_CAUSE_FIELD from '@salesforce/schema/VoiceCall.RootCause__c';
+import OUTCOME_FIELD from '@salesforce/schema/VoiceCall.Outcome__c';
+const VOICE_CALL_FIELDS = [RELATED_RECORD_FIELD,PREVIOUS_CALL_ID_FIELD,DIVISION_FIELD,NEXT_CALL_ID_FIELD,CALL_STATUS_FIELD,CONSIGNMENT_FIELD,TRACKING_NUMBER_FIELD,RELATED_RECORD_ID_FIELD,PRODUCT_CATEGORY_FIELD,PRODUCT_SUB_CATEGORY_FIELD,CONTACT_FIELD,ENQUIRY_TYPE_FIELD,ENQUIRY_SUB_TYPE_FIELD,ROOT_CAUSE_FIELD,OUTCOME_FIELD];
 
 import CASE_NUMBER_FIELD from '@salesforce/schema/Case.CaseNumber';
 import SUBJECT_FIELD from '@salesforce/schema/Case.Subject';
 import STATUS_FIELD from '@salesforce/schema/Case.Status';
 import TYPE_FIELD from '@salesforce/schema/Case.Type';
-const CASE_FIELDS = [CASE_NUMBER_FIELD, SUBJECT_FIELD, STATUS_FIELD, TYPE_FIELD];
+import CASE_PRODUCT_CATEGORY_FIELD from '@salesforce/schema/Case.ProductCategory__c';
+const CASE_FIELDS = [CASE_NUMBER_FIELD, SUBJECT_FIELD, STATUS_FIELD, TYPE_FIELD, CASE_PRODUCT_CATEGORY_FIELD];
 
 const IN_PROGRESS_VOICE_STATUS = 'in-progress';
 
@@ -31,7 +39,7 @@ export default class UnifiedVoiceCallFieldSync extends LightningElement {
 
     voiceCallFields = VOICE_CALL_FIELDS;
 
-    @wire(getRecord, { recordId: '$recordId', fields: '$voiceCallFields'})
+    @wire(getRecord, { recordId: '$recordId', fields: '$voiceCallFields' })
     voiceCallRecord({ error, data }) {
         if (data) {
             this.relatedCaseId = getFieldValue(data, RELATED_RECORD_FIELD);
@@ -43,13 +51,13 @@ export default class UnifiedVoiceCallFieldSync extends LightningElement {
         }
     }
 
-    @wire(getRecord, { recordId: '$relatedCaseId', fields: CASE_FIELDS})
+    @wire(getRecord, { recordId: '$relatedCaseId', fields: CASE_FIELDS })
     caseRecord({ error, data }) {
-        if (data) {
+		if (data) {
             CASE_FIELDS.forEach((field)=>{
-                this.caseDetails[field.fieldApiName] = getFieldValue(data, field);
+				const newFieldValue = getFieldValue(data, field);
+				this.caseDetails[field.fieldApiName] = newFieldValue;
             });
-            console.log('Case data changes:', this.caseDetails);
         } else if (error) {
             console.error('Error retrieving case details:', reduceErrors(error).join(','));
         }
@@ -82,18 +90,19 @@ export default class UnifiedVoiceCallFieldSync extends LightningElement {
 
     processChangeEvent(changeEvent) {
         try {
-            const recordIds = changeEvent.data.payload.ChangeEventHeader.recordIds; // this is the Id of the subsequent record, i.e the one that initiated the change
-            const changePayload = changeEvent.data.payload;
-            const fieldApiNames = [];
-            VOICE_CALL_FIELDS.forEach((field)=>{
-                fieldApiNames.push(field.fieldApiName);
-            });
-
-            if (recordIds.includes(this.recordId)) {
-                notifyRecordUpdateAvailable([{recordId: this.recordId}]);
-            }
+			const recordIds = changeEvent.data.payload.ChangeEventHeader.recordIds; // this is the Id of the subsequent record, i.e the one that initiated the change
+			const changeType = changeEvent.data.payload.ChangeEventHeader.changeType; // this is the Id of the subsequent record, i.e the one that initiated the change
+			if (recordIds.includes(this.recordId) && changeType === 'UPDATE') {
+				const changePayload = changeEvent.data.payload;				
+				const monitorFields = VOICE_CALL_FIELDS.map(field => field.fieldApiName);
+				let hasMatchingField = changePayload.ChangeEventHeader.changedFields.some((field) => {
+                    if (monitorFields.includes(field)) {
+                        notifyRecordUpdateAvailable([{recordId: this.recordId}]);
+                    }
+                });
+			}
         } catch (err) {
-            console.error(reduceErrors(error).join(','));
+            console.error(err);
         }
     }
 }
