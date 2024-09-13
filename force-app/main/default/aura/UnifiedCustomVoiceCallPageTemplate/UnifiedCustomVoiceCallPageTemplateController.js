180a970645967c231c31473d90c76ccd48da60f7
Paul Perry <paul.perry5@auspost.com.au>
2024-09-13 10:09:39 +1000

    Code Review Digest adjustments

--- force-app/main/default/aura/UnifiedCustomVoiceCallPageTemplate/UnifiedCustomVoiceCallPageTemplateController.js
+++ force-app/main/default/aura/UnifiedCustomVoiceCallPageTemplate/UnifiedCustomVoiceCallPageTemplateController.js
@@ -1,8 +1,8 @@
 ({
-    doInit: function(component, event, helper) {
-        let windowUrl = window.location.href;
-        if (!windowUrl.includes('flexipageEditor')) {
-            component.set('v.activeSections', []); // automatically collapse the accordion if not in flexipage editor mode
-        }
-    }
+	doInit: function(component, event, helper) {
+		let windowUrl = window.location.href;
+		if (!windowUrl.includes('flexipageEditor')) {
+			component.set('v.activeSections', []); // automatically collapse the accordion if not in flexipage editor mode
+		}
+	}
 })
