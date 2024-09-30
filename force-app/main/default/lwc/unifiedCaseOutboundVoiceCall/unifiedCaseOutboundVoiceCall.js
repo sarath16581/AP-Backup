e60972ef34c540d0ad594452c298a49bc1e7dfea
StephenL <stephen.liu25@auspost.com.au>
2024-09-30 17:43:19 +1000

    CSLU-1088 addressing pr feedback
    - added more comment
    - added logic to update the related record id only when a case tab is found

--- force-app/main/default/lwc/unifiedCaseOutboundVoiceCall/unifiedCaseOutboundVoiceCall.js
+++ force-app/main/default/lwc/unifiedCaseOutboundVoiceCall/unifiedCaseOutboundVoiceCall.js
@@ -2,7 +2,7 @@
  * @description A LWC component for automatically linking a case when agents initiate an outbound call from Case
  * @author: SteveL
  * @changelog:
- * 2024-09-26 - SteveL - Created
+ * 2024-09-26 - SteveL - CSLU-1088 SCV - Outbound Call - Created
  */
 import { LightningElement,api, wire } from 'lwc';
 import { getRecord, updateRecord, getFieldValue, notifyRecordUpdateAvailable } from 'lightning/uiRecordApi';
@@ -11,8 +11,9 @@ import { IsConsoleNavigation, getAllTabInfo } from 'lightning/platformWorkspaceA
 // fields needed to identify Voice Call as Outbound
 import VOICE_CALL_TYPE from '@salesforce/schema/VoiceCall.CallType';
 import VOICE_CALL_STATUS from '@salesforce/schema/VoiceCall.CallDisposition';
+import VOICE_CALL_RELATED_RECORD from '@salesforce/schema/VoiceCall.RelatedRecordId';
 
-const VOICE_CALL_FIELDS = [VOICE_CALL_TYPE,VOICE_CALL_STATUS];
+const VOICE_CALL_FIELDS = [VOICE_CALL_TYPE,VOICE_CALL_STATUS,VOICE_CALL_RELATED_RECORD];
 
 // field values that can be used to identify a Voice Call as outbound
 const VOICE_CALL_IN_PROGRESS_STATUS = 'in-progress';
@@ -51,7 +52,7 @@ export default class UnifiedCaseOutboundVoiceCall extends LightningElement {
 				let caseTab = tabsInfo.find((tab) => {
 					return tab?.recordId?.startsWith('500');
 				});
-				if (caseTab) {
+				if (caseTab && caseTab.recordId != this.voiceCallDetails.RelatedRecordId) { // only update the RecordRecordId when it is empty or have a different value
 					this.updateVoiceRecord(caseTab.recordId);
 				}
 			});
